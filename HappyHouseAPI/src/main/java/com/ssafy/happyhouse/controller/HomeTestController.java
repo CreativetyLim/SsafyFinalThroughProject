7584abd86b338308c1faeaa4ecbaf4d528ff771a
JayPark <jay@JayParkui-MacBookPro.local>
2020-06-10 22:39:13 +0900

    in resources

---
+++ HappyHouseAPI/src/main/java/com/ssafy/happyhouse/controller/HomeTestController.java
@@ -0,0 +1,16 @@
+package com.ssafy.happyhouse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/qna")
+public class HomeTestController {
+	
+	@GetMapping("/index")
+	public String in() {
+		return "index";
+	}
+	
+}
