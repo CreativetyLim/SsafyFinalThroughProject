cc4e0bbea3a4f95d25c6f6da9f2dad68b800bf08
JoonYoung <moter2588@gmail.com>
2020-06-17 12:33:54 +0900

    Footer Header Main change

--- happyhouse/src/components/Header.vue
+++ happyhouse/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="danger">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand to="/">HappyHouse</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
